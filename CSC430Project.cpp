399a54ff7bf4b8260f3e8cefc287a20e9ab8b330
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-10-19 21:30:55 -0400

    Update CSC430Project.cpp

--- CSC430Project.cpp
+++ CSC430Project.cpp
@@ -1,28 +1,53 @@
+
 // CSC430Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 #include "Bot.h"
+#include "UserManager.h"
 
 using namespace std;
 
 int main()
 {
-
-	UserStates currentState = none;
-
-	Bot* bot = new Bot(currentState);
-	
-	string input;
-	cout << "Welcome to File Bot! Please enter a command to get started." << endl;
-	while(getline(cin,input))
+	while (1)
 	{
-		string result = bot->Listen(input);
-		cout << result << endl;
+
+		cout << "Welcome to File Bot! Please register or log in with an existing account." << endl;
+		cout << "1.Sign Up" << endl;
+		cout << "2.Sign In" << endl;
+
+		int userChoice;
+		cin >> userChoice;
+		cin.ignore();
+
+		UserManager newUserManager;
+		switch (userChoice)
+		{
+		case 1:
+			newUserManager.signUp();
+			continue;
+		case 2:
+			newUserManager.signIn();
+
+			UserStates currentState = none;
+
+			Bot* bot = new Bot(currentState);
+
+			string input;
+			cout << "Please enter a command to get started." << endl;
+			while (getline(cin, input))
+			{
+				string result = bot->Listen(input);
+				cout << result << endl;
+			}
+
+			delete bot;
+			break;
+		}
 	}
-
-	delete bot;
+	
 	return 0;
-}
 
+}
