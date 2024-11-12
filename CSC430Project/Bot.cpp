f2fc69df16b760ab3e297fad0ff291cde79f7a5f
HarithIlang <harithfernando09@gmail.com>
2024-11-11 19:31:01 -0500

    Update Bot.cpp and CSC430Project.cpp with changes from BotAndDB

---
+++ CSC430Project/Bot.cpp
@@ -0,0 +1,110 @@
+#include "Bot.h"
+
+#include <iostream>
+#include <windows.h>
+#include "MessageHolder.h"
+#include <regex>
+
+#include "FindFile.h"
+
+using namespace std;
+
+string Bot::Listen(string& text)
+{
+	//here we create an auto pointer because parse string is returning a safe pointer. to MessageHolder
+	auto currentMessage = ParseString(text);
+	//check if current message exists, if not we don't want an nullptr exception
+	if (currentMessage) {
+		//check if we have a valid command
+		if (isCommand(currentMessage->GetCommand()))
+		{
+			//check the map again to find the exact command we should run
+			auto it = Commands.find(currentMessage->GetCommand());
+			if (it != Commands.end())
+			{
+				//set the state to the value of the map key.
+				SetState(it->second);
+				string message = currentMessage->GetActualMessage();
+				//run the command
+				executeCommand(message);
+
+				// Return "succeeded" only if the message is not empty
+				return message.empty() ? "" : "succeeded";
+			}
+		}
+		else
+		{
+			return "That command doesn't exist. Available commands: /find";
+		}
+	}
+	return "There was a problem with the current message sent.  Please ensure the command is entered in lowercase. Available commands: /find";
+
+}
+
+void Bot::SetState(UserStates newState)
+{
+	currentState = newState;
+}
+
+unique_ptr<MessageHolder> Bot::ParseString(string& text)
+{
+	//regex for the command + message
+	regex reg(R"(/([a-z]+)\s*(.*))");
+	smatch matches;
+	//search the string for the match. if it succeeds then we have a /command message 
+	if(regex_search(text,matches,reg))
+	{
+		string command = matches[1];
+		string message = matches[2];
+		//create smart pointer to MessageHolder
+		return make_unique<MessageHolder>(command, message);
+	}
+	return nullptr;
+}
+
+bool Bot::isCommand(const string& currentCommand)
+{
+	//check if it is a command
+	return Commands.find(currentCommand) != Commands.end();
+}
+
+void Bot::executeCommand(const string& input)
+{
+	switch(currentState)
+	{
+	case findfile:
+
+		if (input.empty()) {  //checks if the filename is empty
+			cout << "Please provide a filename and its extension to search for.(eg. /find filename.txt)" << endl;
+			break;  // Exit the case without executing the search
+		}
+
+
+		cout << "running find command" << endl;
+
+		vector<string> foundfiles;
+
+		string directory = "C:";
+
+
+		//smart pointer so that if lets say it is accidentally skipped, the memory will still be freed.
+		std::unique_ptr<FindFile> fileSearch = std::make_unique<FindFile>(directory, input, foundfiles);
+
+		fileSearch->execute();
+
+		if(foundfiles.size() == 0)
+		{
+			cout << "No files found: " << endl;
+		}
+		else
+		{
+			cout << "files found" << endl;
+			for (const auto& foundfile : foundfiles)
+			{
+				cout << foundfile << endl;
+			}
+		}
+
+		break;
+	}
+}
---
+++ CSC430Project/CSC430Project.cpp
@@ -0,0 +1,54 @@
+
+// CSC430Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "Bot.h"
+#include "UserManager.h"
+
+
+using namespace std;
+
+int main()
+{
+	while (1)
+	{
+
+		cout << "Welcome to File Bot! Please choose an option to get started." << endl;
+		cout << "Press 1 to Sign Up (Create a new account)" << endl;
+		cout << "Press 2 to Sign In (Log in with an existing account)" << endl;
+
+		int userChoice;
+		cin >> userChoice;
+		cin.ignore();
+
+		UserManager newUserManager;
+		switch (userChoice)
+		{
+		case 1:
+			newUserManager.signUp();
+			continue;
+		case 2:
+			newUserManager.signIn();
+
+			UserStates currentState = none;
+
+			Bot* bot = new Bot(currentState);
+
+			string input;
+			cout << "Please enter a command to get started.(Available Commands: /find)" << endl;
+			while (getline(cin, input))
+			{
+				string result = bot->Listen(input);
+				cout << result << endl;
+			}
+
+			delete bot;
+			break;
+		}
+	}
+	
+	return 0;
+
+}
