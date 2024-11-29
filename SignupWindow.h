396da63748867cd21a04dc78f5fe32a8fce45efc
HarithIlang <harithfernando09@gmail.com>
2024-11-29 17:52:07 -0500

    Integrated StartWindow and SignupWindow files from SV-GUI branch while preserving history

---
+++ SignupWindow.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <windows.h>
+#include "DatabaseManager.h"
+
+class SignupWindow {
+public:
+	static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);//processes messages sent to the window
+	void signWin(HINSTANCE hInstance,int nCmdShow);//Display window
+
+private:
+	HWND hSignUp;
+	HWND hSignIn;
+
+	void OpenSignUp(HWND parentHwnd);
+	void OpenSignIn(HWND parentHwnd);
+};
