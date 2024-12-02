4422c6db9228dfe83c177055c174d425bc863a55
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-12-01 19:01:58 -0500

    new database

---
+++ DatabaseAPI.h
@@ -0,0 +1,16 @@
+#include <string>
+
+using namespace std;
+
+class DatabaseAPI
+{
+public:
+	bool UserLoginAuthentication(const string& username, const string& password);
+	bool UserSignUp(const string& username, const string& password, const string& email);
+
+	// Callback function to capture the response
+	static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response);
+
+private:
+	const string apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwZGx1d2hmeGZtbHNxZnBib3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5OTAwMDksImV4cCI6MjA0NzU2NjAwOX0.AbjEaGCgHtLReH_rO_XTerk-EgwrDcBZpisov061Sfs";
+};
