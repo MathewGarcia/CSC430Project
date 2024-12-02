c8e3c721c8bc10e5386dfa7995c80ed6601ce517
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-12-02 01:01:39 -0500

    Update UserManager.h

--- UserManager.h
+++ UserManager.h
@@ -1,16 +1,10 @@
 #pragma once
-#include "USER.h"
-#include "DatabaseManager.h"
 
 using namespace std;
 
 class UserManager
 {
 public:
-    void signUp();
-    void signIn();
-    bool checkUserExists(const string& username, const string& email); // Moved to public section
-
-private:
-    DatabaseManager newDBmanager;
+	void signUp();
+	void signIn();
 };
